77e80d168d36e27d88e804d5281ba27276f12045
sombriks <sombriks@gmail.com>
2019-05-11 13:48:59 -0300

    fix typo

--- src/main/java/br/com/casamagalhaes/panamah/sdk/nfe/DetEvento.java
+++ src/main/java/br/com/casamagalhaes/panamah/sdk/nfe/DetEvento.java
@@ -3,52 +3,42 @@ package br.com.casamagalhaes.panamah.sdk.nfe;
 import java.util.List;
 
 public class DetEvento {
-	
+
 	private String descEvento;
 	private String nProt;
 	private String xJust;
-	private List<Det>det;
+	private List<Det> det;
 
-	/**
-	 * @return the descEvento
-	 */
 	public String getDescEvento() {
 		return descEvento;
 	}
 
-	/**
-	 * @param descEvento the descEvento to set
-	 */
 	public void setDescEvento(String descEvento) {
 		this.descEvento = descEvento;
 	}
 
-	/**
-	 * @return the nProt
-	 */
 	public String getnProt() {
 		return nProt;
 	}
 
-	/**
-	 * @param nProt the nProt to set
-	 */
 	public void setnProt(String nProt) {
 		this.nProt = nProt;
 	}
 
-	/**
-	 * @return the xJust
-	 */
 	public String getxJust() {
 		return xJust;
 	}
 
-	/**
-	 * @param xJust the xJust to set
-	 */
 	public void setxJust(String xJust) {
 		this.xJust = xJust;
+	}
+
+	public List<Det> getDet() {
+		return det;
+	}
+
+	public void setDet(List<Det> det) {
+		this.det = det;
 	}
 
 }
