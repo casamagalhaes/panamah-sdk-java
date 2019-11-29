940ab1c3e4d9226f4cbb6cd55287521ac0a4502c
sombriks <sombriks@gmail.com>
2019-11-29 14:47:07 -0300

    experimentos de performance

--- src/test/java/br/com/casamagalhaes/panamah/sdk/SampleRefresh.java
+++ src/test/java/br/com/casamagalhaes/panamah/sdk/SampleRefresh.java
@@ -1,18 +1,12 @@
 package br.com.casamagalhaes.panamah.sdk;
 
-import br.com.casamagalhaes.panamah.sdk.model.PanamahEan;
-
 public class SampleRefresh {
 
 	public static void main(String[] args) throws Exception {
 		PanamahConfig config = PanamahConfig.fromProperties("teste");
-		
-		PanamahStream p = PanamahStream.init(config);
-	
-		System.out.println(("pressione qualquer tecla para encerrar"));
-
 		config.setDelay(100);
-
+		PanamahStream.init(config);
+		System.out.println(("pressione qualquer tecla para encerrar"));
 		System.in.read();
 	}
 
